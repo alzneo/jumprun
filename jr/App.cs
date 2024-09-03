f72bfabce9ab0a97bcb68a10a546a1becc1a333a
alzneo <25908453+alzneo@users.noreply.github.com>
2024-09-03 20:09:05 +0500

    Add project files. Add .gitattributes and .gitignore.

---
+++ jr/App.cs
@@ -0,0 +1,69 @@
+﻿// TO DO:
+// * Handle link arguments and icon
+// * Handle .lnk with custom targets (UWP/Store apps)
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Shell;
+
+namespace jr
+{
+    public class App : Application
+    {
+        static string[] extensions = new string[] { ".exe", ".bat", ".cmd" };
+
+        [STAThread]
+        public static void Main() => new App().Run();
+
+        public App()
+        {
+            var dir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Apps");
+            if (!Directory.Exists(dir))
+            {
+                dir = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            }
+
+            var list = new JumpList();
+
+            foreach (var i in GetExecutableItems(dir))
+            {
+                list.JumpItems.Add(i);
+            }
+
+            JumpList.SetJumpList(this, list);
+
+            this.Shutdown();
+        }
+
+        IEnumerable<JumpTask> GetExecutableItems(string dir)
+        {
+            var category = new DirectoryInfo(dir).Name;
+            foreach (var i in Directory.GetFiles(dir))
+            {
+                var item = i;
+                var ext = Path.GetExtension(item).ToLowerInvariant();
+
+                if (ext == ".lnk")
+                {
+                    item = NativeMethods.ResolveShortcut(i);
+                    ext = Path.GetExtension(item).ToLowerInvariant();
+                }
+
+                if (!extensions.Contains(ext))
+                    continue;
+
+                var title = Path.GetFileNameWithoutExtension(i);
+                yield return CreateTask(title, item, category);
+            }
+            yield break;
+        }
+
+        JumpTask CreateTask(string title, string path, string category)
+        {
+            return new JumpTask() { Title = title, ApplicationPath = path, CustomCategory = category };
+        }
+    }
+}
---
+++ jr/readme.md
@@ -0,0 +1,26 @@
+﻿jumprun
+=======
+
+Simple launcher for Windows 7 and newer (.NET 4)
+
+Configuration
+-------------
+
+1. Create folder somewhere and put 'jr.exe' in it.
+2. Create subfolder 'Apps' with all shortcuts you want
+3. Pin to taskbar or start menu (drag&drop or through context menu)
+
+Usage
+-----
+
+1. Left click to update list
+2. Right click to access the app list
+
+To Do
+-----
+
+- [ ] Icons on Windows 7
+- [ ] Shortcuts with arguments
+- [ ] Windows Store apps shortcuts
+- [ ] Binary release
+- [ ] Add some screenshots
